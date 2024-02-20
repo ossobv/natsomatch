857298044d9d28ae4c56375244419d492b11c544
Walter Doekes <walter+github@wjd.nu>
2024-02-20 12:02:06 +0100

    Refactor. Move stuff out of main

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::time::{Duration, Instant};
 
 use async_nats::{self, jetstream};
 use async_nats::header::NATS_MESSAGE_ID;
+use bytes::Bytes;
 use futures::StreamExt; // needed for subscription.next()
 use tokio::time::sleep;
 
@@ -101,6 +102,84 @@ impl std::fmt::Display for Stats {
 }
 
 
+struct Input {
+    sub: async_nats::Subscriber,
+}
+
+impl Input {
+    async fn from_config(input: &config_parser::NatsConfig) -> Result<Input, Box<dyn std::error::Error>> {
+        println!("Connecting to NATS (input) {} ...", input.server);
+        let nc_in = connect_nats("nats2jetstream-rs-nats-input", &input.server, &input.tls).await?;
+        let subscription = nc_in.subscribe(input.subject.to_subject()).await?;
+
+        println!("Connected to NATS server INPUT+SUB {:?}", nc_in);
+        println!("- subscription ({}): {:?}", input.subject, subscription);
+        println!("");
+
+        Ok(Input {
+            sub: subscription,
+        })
+    }
+
+    async fn next(&mut self) -> Option<async_nats::Message> {
+        self.sub.next().await
+    }
+}
+
+
+struct Sink {
+    jsctx: async_nats::jetstream::context::Context,
+}
+
+impl Sink {
+    async fn from_config(sink: &config_parser::JetStreamConfig) -> Result<Sink, Box<dyn std::error::Error>> {
+        println!("Connecting to NATS (sink) {} ...", sink.server);
+        let nc_out = connect_nats("nats2jetstream-rs-jetstream-sink", &sink.server, &sink.tls).await?;
+        let js = jetstream::new(nc_out);
+
+        // XXX: move to configparser
+        let js_subjects: Vec<String> = sink.subject_any.split(',').map(|s| s.to_string()).collect();
+
+        println!("Setting up JetStream stream {} {:?} ...", sink.name, js_subjects);
+        let stream_info = js.get_or_create_stream(jetstream::stream::Config {
+            name: sink.name.to_string(),
+            //max_bytes: 5 * 1024 * 1024 * 1024,
+            //storage: StorageType::Memory,
+            subjects: js_subjects,
+            ..Default::default()
+        }).await?;
+
+        println!("Connected to NATS server SINK+JS {:?}", js);
+        println!("- stream info: {:?}", stream_info);
+        println!("");
+
+        Ok(Sink {
+            jsctx: js,
+        })
+    }
+
+    async fn publish_unique<S: async_nats::subject::ToSubject>(
+        &self,
+        subject: S,
+        unique_id: &str,
+        payload: Bytes
+    ) -> Result<async_nats::jetstream::context::PublishAckFuture, async_nats::jetstream::context::PublishError> {
+        // Message headers are used in a variety of JetStream
+        // contexts, such de-duplication, auto-purging of
+        // messages, metadata from republished messages, and
+        // more.
+        // https://docs.nats.io/nats-concepts/jetstream/headers
+        // If we have a proper ID, this deduplicates messages
+        // when we're running multiple importers at the same
+        // time.
+        let mut headers = async_nats::HeaderMap::new();
+        headers.insert(NATS_MESSAGE_ID, unique_id.as_ref());
+
+        self.jsctx.publish_with_headers(subject, headers, payload).await
+    }
+}
+
+
 async fn connect_nats(_name: &str, server: &str, maybe_tls: &Option<config_parser::TlsConfig>) -> Result<async_nats::Client, async_nats::ConnectError> {
     let mut options = async_nats::ConnectOptions::new();
 
@@ -147,43 +226,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //println!("Appconfig {app_config:?}");
 
-    println!("Connecting to NATS (sink) {} ...", app_config.sink.server);
-    let nc_out = connect_nats(
-        "nats2jetstream-rs-jetstream-sink",
-        &app_config.sink.server,
-        &app_config.sink.tls).await?;
-    let js = jetstream::new(nc_out);
-
-    // XXX: move to configparser
-    let js_subjects: Vec<String> = app_config.sink.subject_any
-        .split(',').map(|s| s.to_string()).collect();
-
-    println!("Setting up JetStream stream {} {:?} ...", app_config.sink.name, js_subjects);
-    let stream_info = js.get_or_create_stream(jetstream::stream::Config {
-        name: app_config.sink.name.to_string(),
-        //max_bytes: 5 * 1024 * 1024 * 1024,
-        //storage: StorageType::Memory,
-        subjects: js_subjects,
-        ..Default::default()
-    }).await?;
-
-    println!("Connected to NATS server SINK+JS {:?}", js);
-    println!("- stream info: {:?}", stream_info);
-    println!("");
-
-    println!("Connecting to NATS (input) {} ...", app_config.input.server);
-    let nc_in = connect_nats(
-        "nats2jetstream-rs-nats-input",
-        &app_config.input.server,
-        &app_config.input.tls).await?;
-    let mut subscription = nc_in.subscribe(app_config.input.subject.to_subject()).await?;
-
-    println!("Connected to NATS server INPUT+SUB {:?}", nc_in);
-    println!("- subscription ({}): {:?}", app_config.input.subject, subscription);
-    println!("");
-
-    // Read message from NATS-subscription, publish to NATS-JetStream
-    let _max_timeout = Duration::from_secs(1);
+    let sink = Sink::from_config(&app_config.sink).await?;
+    let mut input = Input::from_config(&app_config.input).await?;
 
     // Stats where we record how we're doing
     let period_stats_1 = Arc::new(Mutex::new(Stats::default()));
@@ -211,38 +255,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Start the main task
     loop {
         let subject_tpl = app_config.sink.subject_tpl.to_string();
-        match subscription.next().await {
+        match input.next().await {
             Some(msg) => {
+                // Prepare
                 let prep_t0 = Instant::now();
-
                 let attrs: payload_parser::BytesAttributes;
                 match payload_parser::BytesAttributes::from_payload(&msg.payload) {
                     Ok(ok) => { attrs = ok; },
                     Err(err) => { eprintln!("payload error: {}; {:?}", err, msg.payload); continue; },
                 }
-                //println!("uniq: {}, attrs: {}", attrs.get_unique_id(), std::str::from_utf8(attrs.attributes).unwrap());
-
-                // Message headers are used in a variety of JetStream
-                // contexts, such de-duplication, auto-purging of
-                // messages, metadata from republished messages, and
-                // more.
-                // https://docs.nats.io/nats-concepts/jetstream/headers
-                // If we have a proper ID, this deduplicates messages
-                // when we're running multiple importers at the same
-                // time.
-                let mut headers = async_nats::HeaderMap::new();
-                headers.insert(NATS_MESSAGE_ID, attrs.get_unique_id().as_ref());
-
-                // In the test setup, get_section_fast() takes about 0us,
-                // while the get_section_safe() takes about 7us.
+                let unique_id = attrs.get_unique_id();
                 let subject = subject_tpl.replace("{section}", attrs.get_section());
                 let prep_td = prep_t0.elapsed();
 
-                // In the test setup, this takes about ?us:
+                // Publish
                 let pub_t0 = Instant::now();
-                let _maybe_ack = js.publish_with_headers(subject, headers, msg.payload).await?;
+                match sink.publish_unique(subject, &unique_id, msg.payload).await {
+                    Ok(_maybe_ack) => {},
+                    Err(err) => { eprintln!("publish error: {}; {}", err, unique_id); continue; },
+                }
                 let pub_td = pub_t0.elapsed();
 
+                // Stats
                 let mut period_stats = period_stats_2.lock().unwrap();
                 period_stats.inc(prep_td, pub_td);
                 drop(period_stats);
