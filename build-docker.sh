5916218a72007eb460870868381fb0c3a0fff747
Walter Doekes <walter+github@wjd.nu>
2025-02-07 11:04:42 +0100

    build: Fix docker build image extraction on newer Docker (25.0+)
    Old format had layer.tar in subdirs.
    New OCI-format has layer blobs in blobs/256/<hash>.
    
    Change: osso-org/changes#545

--- build-docker.sh
+++ build-docker.sh
@@ -5,5 +5,17 @@ sudo docker build --build-arg="GIT_VERSION=$(git describe --always --dirty=-modi
 tmp=$(mktemp -d)
 trap 'rm -rf "$tmp"' EXIT
 sudo docker image save build-natsomatch | tar -C "$tmp" -x
-find "$tmp" -type f -name layer.tar -print0 | xargs -0 tar -xvf
+
+if test -f "$tmp/repositories"; then
+    layertar=$(jq -r '.["build-natsomatch"].latest' <"$tmp/repositories")
+    if test -n "$layertar"; then
+        layertar=$tmp/blobs/sha256/$layertar
+    fi
+else
+    layertar=$(find "$tmp" -type f -name layer.tar)
+fi
+if test -z "$layertar"; then
+    echo "$0: could not parse docker layout to extract output" >&2
+fi
+tar -xvf "$layertar"
 ls -l ./natsomatch
