c8ec56b5baa488260301b709c892ea858f03f1ca
Walter Doekes <walter+github@wjd.nu>
2026-04-16 14:26:59 +0200

    version: Bump to 0.4.0 -- upgrade deps

--- benches/bogus.rs
+++ benches/bogus.rs
@@ -1,4 +1,5 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::hint::black_box;
 
 
 fn test_bogus(input: i32) -> i32 {
